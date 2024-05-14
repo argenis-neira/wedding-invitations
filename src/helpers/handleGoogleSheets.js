abba4d4c31fb03e052ef38a9e01f32e408171d8a
argenis-neira <neira.carlos.argenis@gmail.com>
2024-05-14 11:22:17 -0500

    no access token

--- src/helpers/handleGoogleSheets.js
+++ src/helpers/handleGoogleSheets.js
@@ -1,6 +1,5 @@
 const HandleGoogleSheets = async (data) => {
-  const accessToken = "ya29.a0AXooCgsb-4QtGFdtgF1gtT1F5isqm2R_Mnz38DKFWYnCp2OSff23UHA1RRxfLD3lCOXCXTFJhxz2q46PQ9aQFQHvgmgnfb-onE-OzDfkj_8vgGiovKGTnQngxBFHSIbZPj1HVQSZRtySAYYR9dlSYGL144O_06el7KqDaCgYKAY4SARMSFQHGX2Mi0oIY7r6Ln3DPFPzRCGxS1g0171";
-
+  const accessToken = ""
   const spreadsheetId = '1rG7n7MocRjBYRlyYoM6uTeGdXumjrrAkO5Ugh-JViYY';
   const sheetName = 'Hoja 1';
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}:append?valueInputOption=USER_ENTERED`;
